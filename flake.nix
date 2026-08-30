63aa55e96c2b70256ced69e674e4a184cb713378
Josh Gustafson <joshgstfsn@gmail.com>
2026-08-30 00:52:33 -0600

    Initial: click flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "click: version-bumped ahead of nixpkgs through a Python package overlay.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-lib = {
+      url = "github:jgus/flake-lib/v1";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, flake-lib, ... }:
+    let
+      pin = import ./pin.nix;
+      inherit (pin) version hash;
+      source = { type = "pypi"; pname = "click"; format = "sdist"; };
+      overlay = final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (pyfinal: pyprev: {
+            click = pyprev.click.overridePythonAttrs (_: {
+              inherit version;
+              doCheck = false;
+              src = pyfinal.fetchPypi { inherit version hash; pname = "click"; };
+            });
+          })
+        ];
+      };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+        in
+        {
+          packages = {
+            click = pkgs.python3.pkgs.click;
+            default = pkgs.python3.pkgs.click;
+            update-version = flake-lib.lib.mkUpdateVersion { inherit pkgs source; buildAttr = "click"; };
+            update-branches = flake-lib.lib.mkUpdateBranches { inherit pkgs source; pinSchema = "pypi"; };
+          };
+        }) // {
+      overlays.default = overlay;
+    };
+}
---
+++ pin.nix
@@ -0,0 +1,4 @@
+{
+  version = "8.5.0";
+  hash = "sha256-ug0gid516gMQ4t3gMWDmyhAAmUf7laGC+bVAIbsnLjQ=";
+}
